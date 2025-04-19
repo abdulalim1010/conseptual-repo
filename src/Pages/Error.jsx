8aedfbb513711729bcbfbb67577f114d4d34b38a
abdulalilm1010 <abdulalim243457@gmail.com>
2025-04-19 21:42:21 +0600

    again add navbar

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
 
 const Error = () => {
   return (
     <div>
+      <Navbar/>
       <h1>error handele</h1>
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,49 +1,51 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, NavLink } from 'react-router';
+import { FaShoppingCart } from "react-icons/fa";
+import { FaBookOpenReader } from "react-icons/fa6";
+
+
 
 const Navbar = () => {
   return (
-   <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-100 shadow-sm">
   <div className="navbar-start">
     <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+      <div tabIndex={0} role="button" className="btn cursor-pointer mr-2 lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
       </div>
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
+        <li><Link>Home</Link></li>
         <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
+          <Link>About</Link>
+        
+            </li>
+            <li>
+
+            </li>
         <li><a>Item 3</a></li>
       </ul>
     </div>
-    <Link to='/' className="btn btn-ghost text-xl">jasgldgjdghj</Link>
+    <Link to='/' className="btn btn-ghost text-xl">Amar time notsto</Link>
   </div>
   <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal  px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
+   
   </div>
-  <div className="navbar-end lg:flex">
-    <a className="btn">Button</a>
+      <div className="navbar-end  hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+      <li><NavLink className={({isActive})=>(isActive?'text-white bg-amber-800':'')} to='/'>Home</NavLink></li>
+      <li>
+       <NavLink className={({isActive})=>(isActive?'text-white bg-yellow-300':'')} to='/about'>About</NavLink>
+      </li>
+      <li><NavLink className={({isActive})=>(isActive?'text-white bg-blue-600':'')} to='/cart'><FaShoppingCart size={20} />
+</NavLink></li>
+      <li><NavLink className={({isActive})=>(isActive?'text-white bg-green-500':'')} to='/favorite'><FaBookOpenReader /> </NavLink></li>
+    </ul> 
+   
   </div>
 </div>
+
   );
 };
 
