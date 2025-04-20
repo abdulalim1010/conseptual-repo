db51f48071054eb1fac4a2afe27a6eaba23cf301
abdulalilm1010 <abdulalim243457@gmail.com>
2025-04-20 14:13:53 +0600

    place the restet button

--- src/components/Phone/PhoneContainer.jsx
+++ src/components/Phone/PhoneContainer.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+
+import { useEffect, useState } from 'react';
 import PhoneCart from '../PhoneCart/PhoneCart';
 
 
-const PhoneContainer = ({phones}) => {
+const PhoneContainer = ({ phones }) => {
+  const [displayPhones, setDiaplayPhones] = useState([false])
+  const [showAll,setShowAll]=useState([])
+  useEffect(() => {
+    if( showAll){
+     setDiaplayPhones(phones)
+    }
+    else {
+       setDiaplayPhones(phones.slice(0,6))
+    }
+  },
+
+[showAll,phones]
+
+
+
+)
   
 console.log(phones)
   return (
@@ -10,10 +27,20 @@ console.log(phones)
       <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-7 mb-8'>
       
         {
-             phones.map(phone=>(<PhoneCart key={phone.id} phone={phone}></PhoneCart>))
+          displayPhones.map(phone => (<PhoneCart key={phone.id} phone={phone}></PhoneCart>))
+          
 }
 
-        <button>show all</button>
+        <button onClick={() => {
+          
+            setShowAll(prv => !prv)
+          
+          if(showAll) window.scrollTo(0 ,0)
+        }
+
+        } className='btn btn-praimary  '>            { showAll ?'showless':'showAll'}
+
+        </button>
     </div>
     </div>
   );
