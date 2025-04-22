28d208d4b6b0ec0d974f14285a49787091cc7d9b
abdulalilm1010 <abdulalim243457@gmail.com>
2025-04-22 23:32:03 +0600

    last item

--- src/components/PhoneDetails/PhoneDetails.jsx
+++ src/components/PhoneDetails/PhoneDetails.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
+import Button from '../Ui/Button';
+<button className="btn btn-dash btn-secondary">Secondary</button>
+import { FaBookmark } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { addFavorite } from '../../utiles';
+
 
 const PhoneDetails = () => {
   const data = useLoaderData();
@@ -11,15 +17,23 @@ const PhoneDetails = () => {
     return <p>Phone not found</p>;
   }
 
-  const { name, image,price,description,  model, brand } = singlePhone;
-
+  const { name, image, price, description, model, brand } = singlePhone;
+  const handleFavorite=()=> {
+    addFavorite(singlePhone)
+}
   return (
-    <div>
+    <div className='border-1 bg-green-100 p-6'>
       <h1>{name}</h1>
       <img className='rounded' src={image} alt={name} />
       <p>{brand}</p>
       <h2>{model}</h2>
-      <p>{description}</p>
+      
+        <p>{description}</p>
+      <div className=' justify-center  flex gap-34'>
+        <div><Button label={<FaBookmark />} /></div>
+        
+    <div>  <Button onClick={handleFavorite}  label={<FaShoppingCart />}/></div>
+      </div>
       <div>
   {Object.entries(price).map(([storage, value]) => (
     <button key={storage} style={{ margin: "5px" }}>
---
+++ src/components/Ui/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Button = ({label}) => {
+  return (
+    <button className="btn btn-dash btn-secondary">{ label}</button>
+
+  );
+};
+
+export default Button;
